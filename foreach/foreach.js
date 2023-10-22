a1d25a98ed4e77bdb114d3eac5b2e8928db5b0d9
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-21 18:26:49 -0600

    Add foreach method

---
+++ foreach/foreach.js
@@ -0,0 +1,28 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i];
+// }
+
+// numbers.forEach(item => console.log(item));
+
+//This prints more args. item, index number and array.
+// numbers.forEach((item, i, arr) => console.log(i, item, arr));
+
+
+//foreach() method --- check MDN for array and string methods.
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+//calling the function to print days --- external function
+// days.forEach(printDays);
+
+// function printDays(i, item) {
+//     console.log(item, i)
+// }
+
+
+//internal anonymous function
+days.forEach(function (item) {
+    console.log(item);
+});
+
