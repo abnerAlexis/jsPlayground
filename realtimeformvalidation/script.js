06a631322ed7916251c923d7408f1c9d2abab532
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-30 19:47:40 -0600

    Add email and password validating functions

--- realtimeformvalidation/script.js
+++ realtimeformvalidation/script.js
@@ -3,12 +3,17 @@
     let emailInput = document.querySelector("#email");
     let passwordInput = document.querySelector("#password");
 
+    //an “@” symbol and a “.” required in the user input
     function validateEmail() {
-        return false;
+        let value = emailInput.value;
+        let hasAtSign = value.indexOf("@") > -1;
+        let hasDot = value.indexOf(".") > -1;
+        return value && hasAtSign && hasDot;
     }
 
     function validatePassword() {
-        return false;
+        let value = passwordInput.value; 
+        return value && value.length >= 8;
     }
 
     function validateForm() {
@@ -19,6 +24,8 @@
         e.preventDefault(); // do not submit to the server.
         if (validateForm()) {
             alert("Success");
+        } else {
+            alert("Password or email not valid.")
         }
     })
 
