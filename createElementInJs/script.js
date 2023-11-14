5bc2c8fcf00cf00ea269c075b8dc80bbb082aed5
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-13 19:17:54 -0700

    Create a div element in side a js class

---
+++ createElementInJs/script.js
@@ -0,0 +1,11 @@
+class CreateElement{
+    createDiv() {
+        let mainContainer = document.getElementById('main-container');
+        let divInMain = document.createElement('div');
+        divInMain.classList = 'greet';
+        divInMain.innerText = "Greetings!";
+        mainContainer.appendChild(divInMain);
+    }
+}
+
+new CreateElement().createDiv();
