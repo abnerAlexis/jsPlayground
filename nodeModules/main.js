b47ad4b362e76797d8534345b823c229d963f2d4
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-16 18:54:56 -0700

    Add square module

--- nodeModules/main.js
+++ nodeModules/main.js
@@ -1,4 +1,11 @@
 const circle = require('./circle.js');
+const square = require('./square.js');
 let r = 3;
+
+//Circle
 //`` are used bcs string includes variables.
 console.log(`Circle with ${r} meters radius, has the area of ${circle.area(r)} meter square and ${circle.circumference(3)} meters circumference.`);
+
+//Square
+let side = 5;
+console.log(`Square with the side of ${side} meters has ${square.squareArea(side)} meters area and ${square.squareCircumference(side)} meters of circumference.`);
---
+++ nodeModules/square.js
@@ -0,0 +1,3 @@
+let side;
+module.exports.squareArea = side => Math.pow(side, 2);
+module.exports.squareCircumference = side => side * 4;
