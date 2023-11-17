1ad897e20912dce1f74ee16e81efb4d6cf2a10a3
Alexis Abner <alexisabner@alexiss-air.lan>
2023-11-16 18:11:20 -0700

    module import practice

---
+++ nodeModules/moduleImport/main.js
@@ -0,0 +1,4 @@
+const circle = require('./circle.js');
+let r = 3;
+//`` are used bcs string includes variables.
+console.log(`Circle with ${r} meters radius, has the area of ${circle.area(r)} meter square and ${circle.circumference(3)} meters circumference.`);
